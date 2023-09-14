bb59f2d05f6844451bba2e2dded13fd6a9cc255c
Sydney Cook <s534340@nwmissouri.edu>
2023-09-14 13:50:55 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,27 @@ function burpees() {
     video.style.display = "block";
     video.load();
 }
+
+//Function to display the plank example video
+function plank() {
+    videoSource.src = "media/plank.mp4"
+    descriptionSource.src = "media/plank-descriptions.vtt";
+    video.style.display = "block";
+    video.load();
+}
+
+//Function to display the mountain climbers example video
+function mountain() {
+    videoSource.src = "media/mc.mp4"
+    descriptionSource.src = "media/mountain-descriptions.vtt";
+    video.style.display = "block";
+    video.load();
+}
+
+//Function to display a promo code
+function discount() {
+    var promo = document.getElementById("special");
+    promo.firstChild.nodeValue = "Promo Code: D25START";
+    promo.style.color = "#ff0000";
+    promo.style.fontSize = "2em";
+}
